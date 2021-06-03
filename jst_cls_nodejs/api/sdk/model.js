b6011c1e067df7bd937f41a17d4078c9589cfc9b
Febrinamandasari95 <85095589+Febrinamandasari95@users.noreply.github.com>
2021-06-03 21:46:19 +0700

    Update model.js

--- jst_cls_nodejs/api/sdk/model.js
+++ jst_cls_nodejs/api/sdk/model.js
@@ -23,7 +23,8 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Febrinamandasari95/febrina41420110039/main/jst_cls_nodejs/public/ex_model/model.json';
+        //const path = 'https://raw.githubusercontent.com/zendi014/bot-jst/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
@@ -40,4 +41,3 @@ async function predict(data){
 module.exports = {
     predict: predict 
 }
-  
