503584c5091d8a8ce48f182efa82bdcad460cf13
Febrinamandasari95 <85095589+Febrinamandasari95@users.noreply.github.com>
2021-06-03 23:19:24 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -38,7 +38,6 @@ async function classify(data){
    try{
         // path load in public access => github
         const path = 'https://raw.githubusercontent.com/Febrinamandasari95/febrina41420110039/main/jst_cls_nodejs/public/cls_model/model.json'
-        //const path = 'https://raw.githubusercontent.com/radyafadhil/bot-jst-41420110085/main/public/ex_model/model.json';
         //const path = 'https://raw.githubusercontent.com/zendi014/bot-jst/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
